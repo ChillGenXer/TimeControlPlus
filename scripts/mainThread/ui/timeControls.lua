bad51862e5f2d60e809a04761f50d483dfb1b1e6
chillgenxer <chillgenxer@gmail.com>
2022-11-14 00:50:09 -0500

    UI work is done for now.

--- scripts/mainThread/ui/timeControls.lua
+++ scripts/mainThread/ui/timeControls.lua
@@ -64,8 +64,8 @@ function mod:onload(timeControls)
         local dayBaseOffset = vec3(12,34,0)                     --offset for the day text control.
         local seasonBaseOffset = vec3(15,25,0)                  --offset for the season image control.
         local seasonCircleBaseOffset = vec3(75.0, 59.0, 1.0)    --offset for the circle panel bookend
-        local seasonCircleBackBaseOffset = vec3(75.0, 59.0, 2.0)
-        local seasonTreeBaseOffset = vec3(87.0, 28.0, 20.0)     --offset for the seasonal tree icon
+        local seasonCircleBackBaseOffset = vec3(75.0, 59.0, 10.0)
+        local seasonTreeBaseOffset = vec3(90.0, 26.0, 20.0)     --offset for the seasonal tree icon
 
         local skyBlue = vec3(0.05,0.2,0.0)
         --Scaling
@@ -82,24 +82,23 @@ function mod:onload(timeControls)
         myMainView.baseOffset = vec3(offsetFromGamePanel, -10.0, 0.0)
         myMainView.size = panelSizeToUse
 
-        --Circlular panel to create a bookend to the clock icon on the vanilla time control
-        local seasonCircle = ModelView.new(myMainView)
-        seasonCircle:setModel(model:modelIndexForName("ui_circleBackgroundSmall"))
-        seasonCircle.relativePosition = ViewPosition(MJPositionInnerLeft, MJPositionBelow)
-        seasonCircle.scale3D = vec3(circleBackgroundScale,circleBackgroundScale,circleBackgroundScale)
-        seasonCircle.size = vec2(circuleBackSize, circuleBackSize)
-        seasonCircle.baseOffset = seasonCircleBackBaseOffset
-        seasonCircle.alpha = 0.9
---[[
+
+        local function mat(key, color, roughness, metal)
+            return {key = key, color = color, roughness = roughness, metal = metal or 0.0}
+        end
+
         --White background for the circular panel
         local seasonCircleBack = ModelView.new(myMainView)
-        seasonCircleBack:setModel(model:modelIndexForName("ui_circleBackgroundLargeOutline"))
+        seasonCircleBack:setModel(model:modelIndexForName("ui_circleBackgroundLargeOutline",
+        {
+            [material.types.ui_background.index] = material.types.ui_background_blue.index,
+            [material.types.ui_standard.index] = material.types.ui_selected.index,
+        }))
         seasonCircleBack.relativePosition = ViewPosition(MJPositionInnerLeft, MJPositionBelow)
         seasonCircleBack.scale3D = vec3(circleBackgroundScale,circleBackgroundScale,circleBackgroundScale)
         seasonCircleBack.size = vec2(circleViewSize, circleViewSize)
         seasonCircleBack.baseOffset = seasonCircleBaseOffset
-        seasonCircleBack.alpha = 0.9
---]]
+        seasonCircleBack.alpha = 0.70
 
         --The background panel for the text.  It is snuggled up to the rightmost edge of the vanilla time control
         local myPanelView = ModelView.new(myMainView)
@@ -110,6 +109,19 @@ function mod:onload(timeControls)
         myPanelView.scale3D = vec3(panelScaleToUseX,panelScaleToUseY,panelScaleToUseX)
         myPanelView.size = panelSizeToUse
         myPanelView.alpha = 0.9     --This affects transparency.
+       
+        --A ModelView to show the tree to represent the season
+        local seasonTreeImage = ModelView.new(myPanelView)
+        seasonTreeImage:setModel(model:modelIndexForName("appleTreeAutumn"))
+        seasonTreeImage.relativePosition = ViewPosition(MJPositionInnerLeft, MJPositionBelow)
+        seasonTreeImage.scale3D = vec3(seasonTreeImageScale,seasonTreeImageScale,seasonTreeImageScale)
+        seasonTreeImage.size = vec2(circleViewSize, circleViewSize)
+        seasonTreeImage.baseOffset = seasonTreeBaseOffset
+        seasonTreeImage.alpha = 1.0
+        seasonTreeImage.update = function(dt)
+            --Update the image based on what season it is.
+            seasonTreeImage:setModel(model:modelIndexForName(getSeason()))
+        end
 
         --The year text, and the update function to keep it refreshed with the correct value
         yearTextView = TextView.new(myPanelView)
@@ -132,7 +144,21 @@ function mod:onload(timeControls)
             --Calculate the day of the year.
             dayTextView.text = "Day  " .. tostring((math.floor(world_:getWorldTime()/world_:getDayLength())) % 8 + 1)
         end
+    end
+end
 
+return mod
+
+--[[      
+        --Circlular panel to create a bookend to the clock icon on the vanilla time control
+        local seasonCircle = ModelView.new(myMainView)
+        seasonCircle:setModel(model:modelIndexForName("ui_circleBackgroundSmall"))
+        seasonCircle.relativePosition = ViewPosition(MJPositionInnerLeft, MJPositionBelow)
+        seasonCircle.scale3D = vec3(circleBackgroundScale,circleBackgroundScale,circleBackgroundScale)
+        seasonCircle.size = vec2(circuleBackSize, circuleBackSize)
+        seasonCircle.baseOffset = seasonCircleBackBaseOffset
+        seasonCircle.alpha = 0.9
+--]]
 
 
         --[[
@@ -153,22 +179,7 @@ function mod:onload(timeControls)
         seasonBackgroundCircle.alpha = 0.9
         --]]
 
-        --A ModelView to show the tree to represent the season
-        local seasonTreeImage = ModelView.new(myPanelView)
-        seasonTreeImage:setModel(model:modelIndexForName("appleTreeAutumn"))
-        seasonTreeImage.relativePosition = ViewPosition(MJPositionInnerLeft, MJPositionBelow)
-        seasonTreeImage.scale3D = vec3(seasonTreeImageScale,seasonTreeImageScale,seasonTreeImageScale)
-        seasonTreeImage.size = vec2(circleViewSize, circleViewSize)
-        seasonTreeImage.baseOffset = seasonTreeBaseOffset
-        seasonTreeImage.alpha = 1.0
-        seasonTreeImage.update = function(dt)
-            --Update the image based on what season it is.
-            seasonTreeImage:setModel(model:modelIndexForName(getSeason()))
-        end
-    end
-end
 
-return mod
 
 --Is this related to my code?  Not sure how this is getting impacted by what I have implemented here.
 --It seems to be after playing for a long time period, memory leak somewhere maybe?
