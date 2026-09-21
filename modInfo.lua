2ee42ebad269bc9c5574e4a0d1604589953b4b57
ChillGenXer <81128365+ChillGenXer@users.noreply.github.com>
2026-09-20 20:40:46 -0400

    Refresh mod description

--- modInfo.lua
+++ modInfo.lua
@@ -1,6 +1,6 @@
 local modInfo = {
   name = "Time Control Plus",
-  description = "An enhancement for the Time Control to add world time and calendar information. Full documentation is located at the website link.",
+  description = "Adds year, world time, and seasonal information to the Time Control HUD, with support for Sapiens 0.6.1.3 and 0.7.0.2.",
   preview = "assets/TimeControlPlus.png",
   version = "0.5.0.1",
   type = "world",
